fbee7b5f9b0923ceeb1dc573336f3ae93d601ecf
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-05-20 07:54:52 +0300

    readme

--- main.h
+++ main.h
@@ -9,6 +9,21 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+extern char **environ;
+
+/**
+ * struct node - linked list of the environment
+ * @data: string
+ * @len: length of the string
+ * @next: points to the next node
+ */
+
+typedef struct node {
+	char *data;
+        unsigned int len;
+	struct node* next;
+} node;
+
 int interactive(char **argv, char **envp);
 int non_interactive(char **argv, char **envp);
 
@@ -27,6 +42,7 @@ int _strncmp(char *s1, char *s2, int ind);
 int _strcmp(char *s1, char *s2);
 int _atoi(char *str);
 char *_strcat(char *dest, char *src);
+char *_strdup(char *str);
 char *tostring(int num);
 
 char *_strtok(char *str, const char *delim);
@@ -35,5 +51,15 @@ char *get_path(char **envp);
 char **get_dirs(char *path);
 char *file_dir(char **dirs, char *file_name);
 void free_arr(char **arr);
+
+node *add_node_end(node **head, char *str);
+size_t print_list(const node *h);
+void free_list(node *head);
+int delete_node_at_index(node **head, unsigned int index);
+char ** get_env(void);
+void _env(node *head)
+char *get_linked_list_var(node *curr_node);
+int set_env(char *var, char *value, node **head);
+int unset_env(char *var, node **head);
 
 #endif
--- string_functions2.c
+++ string_functions2.c
@@ -57,3 +57,28 @@ char *tostring(int num)
 
 	return (str);
 }
+
+/**
+ * _strdup - dupplicate the string
+ * @str: string
+ * Return: string
+ */
+
+char *_strdup(char *str)
+{
+	int i, ii, len = 0;
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	dup = (char *) malloc(len + 1 * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+	for (ii = 0; ii < len; ii++)
+		dup[ii] = str[ii];
+	dup[ii] = '\0';
+	return (dup);
+}
+
